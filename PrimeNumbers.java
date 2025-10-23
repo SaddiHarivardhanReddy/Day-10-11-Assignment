a2bd9ab76cc02d37be323724d22223f3bb3f10b5
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-23 23:40:29 +0530

    Prime numbers

---
+++ PrimeNumbers.java
@@ -0,0 +1,21 @@
+package com.codegnan.loopingstatements;
+import java.util.Scanner;
+public class PrimeNumbers {
+public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			int n=s.nextInt();
+			for (int i = 2; i <= n; i++) {
+	            int count = 0;
+	            for (int j = 1; j <= i; j++) {
+	                if (i % j == 0) {
+	                    count++;
+	                }
+	            }
+	            if (count == 2) {
+	                System.out.print(i + " ");
+	            }
+	        }
+
+	        s.close();	}
+
+	}
