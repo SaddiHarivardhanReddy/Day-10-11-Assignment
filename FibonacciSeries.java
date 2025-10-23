c93bdae0d50694cbb229aa56dd2a6d523c4be348
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-23 23:38:13 +0530

    Fibonacci series

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+package com.codegnan.loopingstatements;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			int n=s.nextInt();
+			int num1=0,num2=1;
+			System.out.print(num1+" "+num2);
+			for(int i=2;i<n;i++) {
+				int num3=num1+num2;
+				System.out.print(" "+num3);
+				num1=num2;
+				num2=num3;
+			}
+			s.close();
+
+	}
+
+}
